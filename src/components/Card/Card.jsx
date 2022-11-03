9db7ed449ec8c97e9dcfb59200bb4035feb740e5
tastyk0yla <107864983+tastyk0yla@users.noreply.github.com>
2022-11-03 21:14:40 +0300

    first commit, base layout made

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,45 @@
+import { Component } from 'react'
+import { parseISO, format } from 'date-fns'
+
+import './Card.css'
+
+export default class Card extends Component {
+  makeItShort = (string) => {
+    const desiredLength = 160
+    if (string.length < desiredLength) return string
+    const arr = string.split(' ')
+    let res = ''
+    for (let i = 0; i < arr.length; i++) {
+      if (res.length + arr[i].length >= desiredLength) break
+      res += arr[i] + ' '
+    }
+    if (res[res.length - 1] === ' ') {
+      res = res.slice(-0, res.length - 1) + '...'
+    } else res += '...'
+    return res
+  }
+
+  render() {
+    const { poster, title, about, release } = this.props
+    const date = format(parseISO(release), 'LLLL d, yyyy')
+    const overview = this.makeItShort(about)
+    return (
+      <li className="card">
+        <img src={`https://image.tmdb.org/t/p/original${poster}`} alt={title} />
+        <div className="card-content">
+          <h2 className="card--title">{title}</h2>
+          <p className="card--date">{date}</p>
+          <ul className="card--genre-list">
+            <li className="genre-list--item">
+              <p>Action</p>
+            </li>
+            <li className="genre-list--item">
+              <p>Drama</p>
+            </li>
+          </ul>
+          <p className="card--about">{overview}</p>
+        </div>
+      </li>
+    )
+  }
+}
---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,30 @@
+import Card from '../Card'
+
+import './CardList.css'
+
+const CardList = (props) => {
+  const { data } = props
+  const { page, items } = data
+  const pagesArr = items.reduce(
+    (acc, item, i) => {
+      const pageIndex = Math.floor(i / 6)
+      if (acc[pageIndex] === undefined) acc.push([])
+      acc[pageIndex].push(
+        <Card
+          key={i}
+          poster={item.poster_path}
+          title={item.original_title}
+          about={item.overview}
+          release={item.release_date}
+          stars={item.vote_average}
+        />
+      )
+      return acc
+    },
+    [[]]
+  )
+
+  return <ul className="card-list">{pagesArr[page - 1]}</ul>
+}
+
+export default CardList
