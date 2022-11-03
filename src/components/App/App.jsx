a48409394dd2e43beaa4955b7af974eed7abf428
tastyk0yla <107864983+tastyk0yla@users.noreply.github.com>
2022-11-03 22:53:47 +0300

    Added spinner, handled errors (including offline)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
-import { Component } from 'react'
+import { Offline, Online } from 'react-detect-offline'
+import { Component, Fragment } from 'react'
+import { Alert, Spin } from 'antd'
 
 import './App.css'
 
@@ -11,6 +13,9 @@ export default class App extends Component {
     this.state = {
       page: 1,
       items: [],
+      isLoading: true,
+      isError: false,
+      error: null,
     }
   }
   api = new apiService()
@@ -19,22 +24,46 @@ export default class App extends Component {
     this.api
       .searchMovies('return')
       .then((res) => {
-        this.setState({ items: res.results })
+        this.setState({ items: res.results, isLoading: false })
       })
       .catch((err) => {
-        console.error(err)
+        this.onError(err)
       })
   }
 
+  onError = (err) => {
+    this.setState({ isLoading: false, isError: true, error: err })
+  }
   componentDidMount() {
     this.getItems()
   }
 
   render() {
+    const { isLoading, isError, error } = this.state
+    const errEl = isError ? (
+      <Alert style={{ margin: 'auto' }} message={error.message} showIcon description={error.stack} type="error" />
+    ) : null
+    const spinner = isLoading ? <Spin size="large" style={{ margin: 'auto' }} /> : null
+    const success = !(isLoading || error) ? <CardList data={this.state} /> : null
     return (
-      <section className="app">
-        <CardList data={this.state} />
-      </section>
+      <Fragment>
+        <Offline>
+          <Alert
+            style={{ margin: 'auto' }}
+            message="You are offline"
+            showIcon
+            description="Check your connection and refresh the page"
+            type="error"
+          />
+        </Offline>
+        <Online>
+          <section className="app">
+            {errEl}
+            {spinner}
+            {success}
+          </section>
+        </Online>
+      </Fragment>
     )
   }
 }
